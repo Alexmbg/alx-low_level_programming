fa876351a7f8035864e37e05d5e3cf469fc91bbe
Alexmbg <99515685+Alexmbg@users.noreply.github.com>
2022-09-08 18:23:10 -0700

    A C program that prints a string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+
 /**
  * main- print the string inthe printf function
  *
- *
  * Description: using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art," 
- * Return: 0
- * 
+ * this program prints with proper grammar, but the outcome is a piece of art, 
+ * Return: 0 
  */
 int main(void)
 {
