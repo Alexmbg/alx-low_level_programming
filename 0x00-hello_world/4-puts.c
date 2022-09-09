8481c409a02051d6cd340fd40e2b7c3d82035b54
Alexmbg <99515685+Alexmbg@users.noreply.github.com>
2022-09-08 17:55:35 -0700

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #!/bin/bash
-
+/**
+ * main-prints string inthe pus function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 #include <stdio.h>
-char str[]="\"Programming is like building a multilingual puzzle"
-puts(str)
-return (0);
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
