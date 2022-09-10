3bbeb690196f63fb6819b3f1b364ff05e0f7a8af
Alexmbg <99515685+Alexmbg@users.noreply.github.com>
2022-09-09 18:54:00 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: print base 16 numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
